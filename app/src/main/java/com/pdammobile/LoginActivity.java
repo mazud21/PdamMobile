338a4c25fac341bc8a108ecfd95f1619c704d076
masud <hmazud@gmail.com>
2019-06-19 21:00:14 +0700

    First Commit

---
+++ app/src/main/java/com/pdammobile/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.pdammobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+    }
+
+}
